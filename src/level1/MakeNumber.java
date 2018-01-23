c2fcba5674922ddc412d6896eb91d669c1d4af83
splendididea <splendid_idea@naver.com>
2018-01-23 23:00:21 +0900

    알고리즘 문제풀이 커밋

---
+++ src/level1/MakeNumber.java
@@ -0,0 +1,19 @@
+package level1;
+/***
+ * 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환해주는 gcdlcm 함수를 완성해 보세요. 
+ * 배열의 맨 앞에 최대공약수, 그 다음 최소공배수를 넣어 반환하면 됩니다. 예를 들어 gcdlcm(3,12) 가 입력되면,
+ * [3, 12]를 반환해주면 됩니다.
+ */
+public class MakeNumber {
+    public int[] gcdlcm(int a, int b) {
+        int[] answer = new int[2];
+        // 재귀 호출 
+        return answer;
+    }
+
+    public static void main(String[] args){
+        MakeNumber mk = new MakeNumber();
+        System.out.println(mk.gcdlcm(3, 12));
+    }
+
+}
