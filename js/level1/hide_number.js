c5bc219008b6723d963db1ef26fd3f98653a1fa0
ohmook <jmookoh@gmail.com>
2018-08-21 17:00:14 +0900

    second

--- js/level1/hide_number.js
+++ js/level1/hide_number.js
@@ -11,23 +11,25 @@ function hide_number(s) {
     // //var result = "*".repeat(s.length - 4) + s.slice(-4);
     // return result;
 
-    /*
-     * @author OhMook
-     * ES5
-     */
-    
-    //case 1
-    return s.substring(0, s.length-4).replace(/\d/g,'*').concat(s.match(/\d{1,4}$/g));
+
+/*
+ * @author OhMook
+ * ES5
+ */
+
+//case 1
+return s.substring(0, s.length-4).replace(/\d/g,'*').concat(s.match(/\d{1,4}$/g));
 
 
-    //case 2
-    var ptn = [];
+//case 2
+var ptn = [];
 
-    return (s.replace(/(\d{1,4})$/, function(cx, $1){
-                ptn = $1; return '';
-            }).replace(/\d/g, '*').concat(ptn));
+return (s.replace(/(\d{1,4})$/, function(cx, $1){
+    ptn = $1; return '';
+}).replace(/\d/g, '*').concat(ptn));
 
-    }
+}
 console.log("결과 :: " + hide_number('01033334444'))
 console.log("결과 :: " + hide_number('0103333444'))
 console.log("결과 :: " + hide_number('0133'))
+w
